5152b11df7c595ec99c7acaf1bc3b7f2aa8087b4
Thomas Holtam <thomasjholtamIII@gmail.com>
2023-12-16 18:25:05 -0700

    Filled out functions and index can generate README

---
+++ generateMarkdown.js
@@ -0,0 +1,69 @@
+// Function to generate the license badge
+function renderLicenseBadge(license) {
+  if (license === 'No License') {
+    return '';
+  }
+  return `[![License Badge](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`;
+}
+
+// Function to give badge a link to the licenses description
+function renderLicenseLink(license) {
+  if (license === 'No License') {
+    return '';
+  }
+  return 'https://opensource.org/licenses/' + license;
+}
+
+
+// Function to add description to License
+function renderLicenseSection(license) {
+  if (license === 'No License') {
+    return 'This project has no license.';
+  }
+  return `${license} license information.`;
+}
+
+
+// Scaffold and Function to make README, this is passed to the Index.js
+function generateMarkdown(data) {
+  const { title, description, installation, usage, license, gitHubLink, email, tests, author } = data;
+
+  return `# ${title}
+${renderLicenseBadge(license)}
+
+## Description
+${description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contributing
+${author}
+
+## License
+${renderLicenseBadge(license)}
+${renderLicenseSection(license)}
+
+## Tests
+${tests}
+
+## Questions
+If you have any questions, feel free to reach out:
+- GitHub: https://github.com/${gitHubLink}
+- Email: ${email}`;
+}
+
+// Export generateMarkdown to be used in index
+module.exports = generateMarkdown;
+
--- index.js
+++ index.js
@@ -1,13 +1,77 @@
-// TODO: Include packages needed for this application
+// Data pulled from other sources that are used in this page
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Function to take data from the generateMarkdown import and the user input to write the file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => (err ? console.log(err) : console.log('Nice!')));
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
+// User question to be answered in command line
+function init() {
+  const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description of your project',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Do you have installation instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How is this project used?',
+      },
+      {
+          type: 'list',
+          name: 'license',
+          message: 'Choose a license for your project:',
+          choices: [
+            'MIT',
+            'GNU GPLv3',
+            'Apache 2.0',
+            'ISC',
+            'BSD 3-Clause',
+            'No License',
+          ],
+        },
+      {
+        type: 'input',
+        name: 'gitHubLink',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email',
+      },
+      {
+          type: 'input',
+          name: 'tests',
+          message: 'How can you test your application?',
+        },
+      {
+          type: 'input',
+          name: 'author',
+          message: 'Please name who contributed, add a comma after names if multiple',
+        },
+    ]
+    // Fead response data back into the Generate Markdown import
+  inquirer.prompt(questions).then((response) => {
+    const markdown = generateMarkdown(response);
+    // Pass the data over to writeToFile to generate the README.md
+    writeToFile('README.md', markdown);
+  });
+}
+// Fire function housing questions
 init();
+
