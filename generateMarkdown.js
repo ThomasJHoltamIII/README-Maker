ae6b2500225376fc8d1860503c05d0547eb0f06d
Thomas Holtam <thomasjholtamIII@gmail.com>
2023-12-16 18:41:33 -0700

    Formatted js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,32 +1,42 @@
 // Function to generate the license badge
 function renderLicenseBadge(license) {
-  if (license === 'No License') {
-    return '';
+  if (license === "No License") {
+    return "";
   }
-  return `[![License Badge](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`;
+  return `[![License Badge](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(
+    license
+  )})`;
 }
 
 // Function to give badge a link to the licenses description
 function renderLicenseLink(license) {
-  if (license === 'No License') {
-    return '';
+  if (license === "No License") {
+    return "";
   }
-  return 'https://opensource.org/licenses/' + license;
+  return "https://opensource.org/licenses/" + license;
 }
-
 
 // Function to add description to License
 function renderLicenseSection(license) {
-  if (license === 'No License') {
-    return 'This project has no license.';
+  if (license === "No License") {
+    return "This project has no license.";
   }
   return `${license} license information.`;
 }
 
-
 // Scaffold and Function to make README, this is passed to the Index.js
 function generateMarkdown(data) {
-  const { title, description, installation, usage, license, gitHubLink, email, tests, author } = data;
+  const {
+    title,
+    description,
+    installation,
+    usage,
+    license,
+    gitHubLink,
+    email,
+    tests,
+    author,
+  } = data;
 
   return `# ${title}
 ${renderLicenseBadge(license)}
@@ -66,4 +76,3 @@ If you have any questions, feel free to reach out:
 
 // Export generateMarkdown to be used in index
 module.exports = generateMarkdown;
-
